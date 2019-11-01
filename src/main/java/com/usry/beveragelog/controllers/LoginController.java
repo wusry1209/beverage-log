3d822c5a4c276cd48b5502781d01bc7fa7f5f910
wusry1209 <connorusry@gmail.com>
2019-11-01 10:13:01 -0500

    Login redirects to admin page without issue. From there add/list added to login page. Add redirects to index

--- src/main/java/com/usry/beveragelog/controllers/LoginController.java
+++ src/main/java/com/usry/beveragelog/controllers/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,12 +22,14 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+
+    @RequestMapping(value={"/login"}, method = RequestMethod.GET)
     public ModelAndView login(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
         return modelAndView;
     }
+
 
 
     @RequestMapping(value="/registration", method = RequestMethod.GET)

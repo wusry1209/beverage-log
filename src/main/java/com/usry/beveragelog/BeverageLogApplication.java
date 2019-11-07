b7bc15097b54b2f7cdd5427a0248ab6756a722ea
wusry1209 <connorusry@gmail.com>
2019-11-07 16:32:08 -0600

    Working file upload form. Need to fix some redirects in controllers.

--- src/main/java/com/usry/beveragelog/BeverageLogApplication.java
+++ src/main/java/com/usry/beveragelog/BeverageLogApplication.java
@@ -1,7 +1,10 @@
 package com.usry.beveragelog;
 
+import com.usry.beveragelog.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BeverageLogApplication {
@@ -9,5 +12,15 @@ public class BeverageLogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeverageLogApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+
+	}
+
 
 }
---
+++ src/main/java/com/usry/beveragelog/storage/StorageService.java
@@ -0,0 +1,26 @@
+package com.usry.beveragelog.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+
+public interface StorageService {
+
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+
+}
